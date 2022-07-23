67eea32d0a2e5712c06bd533e549e6125c0a066c
Sunny-Sea <663253062@qq.com>
2022-07-23 17:16:19 +0800

    使用 create-react-app 脚手架

---
+++ react-learn/src/index.jsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: Sunny-Sea 663253062@qq.com
+ * @Date: 2022-07-23 16:46:39
+ * @LastEditors: Sunny-Sea 663253062@qq.com
+ * @LastEditTime: 2022-07-23 17:09:01
+ * @FilePath: /React/react-learn/src/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <h1>
+    hello world <span>元素</span>
+  </h1>,
+  document.getElementById("root")
+);
