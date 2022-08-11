c6a86e3650f559e61b7f27af59fcdbb3ff1cecf7
Sunny-Sea <663253062@qq.com>
2022-08-11 15:14:46 +0800

    demo自动移动的小球

--- react-learn/src/components/studentList.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import Student from "./student";
-
-export default class StudentList extends Component {
-    render() {
-        const students = this.props.stus.map((item) => <Student key={item.id} {...item} />);
-        return <ul>{students}</ul>;
-    }
-}
