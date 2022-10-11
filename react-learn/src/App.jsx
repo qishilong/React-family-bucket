8411f57a789180e893f1c832d61c1ef241e9d90a
qishilong <663253062@qq.com>
2022-10-11 13:49:42 +0800

    提交demo表单组合

--- react-learn/src/App.jsx
+++ react-learn/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
-import NewContext from "./Context/NewContext/NewContext";
+// import NewContext from "./Context/NewContext/NewContext";
+import Test from "./components/common/Form/Test";
 
 class App extends Component {
     render() {
         return (
             <>
-                <NewContext />
+                <Test />
             </>
         );
     }
