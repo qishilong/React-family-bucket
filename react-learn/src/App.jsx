8511c12ddff3332d09ff024d2ccc9af72e648baa
qishilong <663253062@qq.com>
2022-10-11 20:54:53 +0800

    提交Portals

--- react-learn/src/App.jsx
+++ react-learn/src/App.jsx
@@ -1,15 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Test from "./RenderProps/Test";
-
-class App extends Component {
-    render() {
-        return (
-            <>
-                <Test />
-            </>
-        );
-    }
+function ChildA() {
+    return ReactDOM.createPortal(
+        <div
+            className="child-a"
+            style={{
+                marginTop: 200,
+            }}
+        >
+            <h1>ChildA</h1>
+            <ChildB />
+        </div>,
+        document.querySelector(".modal"),
+    );
 }
 
-export default App;
+function ChildB() {
+    return (
+        <div className="child-b">
+            <h1>ChildB</h1>
+        </div>
+    );
+}
+
+export default function App() {
+    return (
+        <div
+            className="app"
+            onClick={e => {
+                console.log("App被点击了", e.target);
+            }}
+        >
+            <h1>App</h1>
+            <ChildA />
+        </div>
+    );
+}
