064ee099cdae8400e93116513664ee100ba77307
qishilong <663253062@qq.com>
2022-10-12 10:11:34 +0800

    提交扩展react中的事件

--- react-learn/src/index.jsx
+++ react-learn/src/index.jsx
@@ -12,3 +12,10 @@ root.render(<App />);
 const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App tab="home" />); */
+document.addEventListener("click", function (e) {
+    e.stopImmediatePropagation(); //阻止剩余的事件处理程序运行
+});
+
+document.addEventListener("click", function (e) {
+    console.log("真实DOM：document 被点击了");
+});
