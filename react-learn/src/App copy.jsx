05abb19e3a4751aa0a1c3442f2360fb0b2896d9b
qishilong <663253062@qq.com>
2022-10-13 16:00:31 +0800

    提交useCallback

--- react-learn/src/App copy.jsx
+++ react-learn/src/App copy.jsx
@@ -1,4 +1,4 @@
-import { PureComponent, useCallback, useState } from "react";
+import { PureComponent, useState } from "react";
 
 class Pure extends PureComponent {
     render() {
@@ -16,15 +16,13 @@ const Parent = () => {
     console.log("Parent Render");
     const [n, setN] = useState(0);
     const [text, setText] = useState(1);
-    const handleClick = useCallback(() => setText(previous => (previous = Math.random())), [text]);
-
     return (
         <>
             {/* react 每次都会使用 Object.is（浅比较） 方法进行比较前后数据的引用是否相同，来决定是否重新渲染组件*/}
             {/* 函数的地址每次渲染都发生了改变，导致子组件跟着渲染，若子组件是经过优化的组件，则可能导致子组件优化失效 */}
             <Pure
                 text={text}
-                onClick={handleClick}
+                onClick={() => setText(previous => (previous = Math.random()))}
             />
             <input
                 type="number"
