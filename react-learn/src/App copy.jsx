6f4e3e2d7bdcdfbe12c35e5feb6e4f3df8b95490
qishilong <663253062@qq.com>
2022-10-13 18:48:59 +0800

    提交useMemo

--- react-learn/src/App copy.jsx
+++ react-learn/src/App copy.jsx
@@ -1,4 +1,4 @@
-import { PureComponent, useState } from "react";
+import { PureComponent, useMemo, useState } from "react";
 
 class Pure extends PureComponent {
     render() {
@@ -16,13 +16,17 @@ const Parent = () => {
     console.log("Parent Render");
     const [n, setN] = useState(0);
     const [text, setText] = useState(1);
+    const handleClick = useMemo(() => {
+        return () => setText(previous => (previous = Math.random()));
+    }, [text]);
+
     return (
         <>
             {/* react 每次都会使用 Object.is（浅比较） 方法进行比较前后数据的引用是否相同，来决定是否重新渲染组件*/}
             {/* 函数的地址每次渲染都发生了改变，导致子组件跟着渲染，若子组件是经过优化的组件，则可能导致子组件优化失效 */}
             <Pure
                 text={text}
-                onClick={() => setText(previous => (previous = Math.random()))}
+                onClick={handleClick}
             />
             <input
                 type="number"
--- react-learn/src/App.jsx
+++ react-learn/src/App.jsx
@@ -1,46 +1,52 @@
-import { PureComponent, useCallback, useState } from "react";
+import { useMemo, useState } from "react";
 
-class Pure extends PureComponent {
-    render() {
-        console.log("Pure Render");
-        return (
-            <div>
-                <p>{this.props.text}</p>
-                <button onClick={this.props.onClick}>改变文本</button>
-            </div>
-        );
-    }
-}
-
-const Parent = () => {
-    console.log("Parent Render");
-    const [n, setN] = useState(0);
-    const [text, setText] = useState(1);
-    const handleClick = useCallback(() => setText(previous => (previous = Math.random())), [text]);
-
-    return (
-        <>
-            {/* react 每次都会使用 Object.is（浅比较） 方法进行比较前后数据的引用是否相同，来决定是否重新渲染组件*/}
-            {/* 函数的地址每次渲染都发生了改变，导致子组件跟着渲染，若子组件是经过优化的组件，则可能导致子组件优化失效 */}
-            <Pure
-                text={text}
-                onClick={handleClick}
-            />
-            <input
-                type="number"
-                value={n}
-                onChange={e => setN(previous => (previous = parseInt(e.target.value)))}
-            />
-        </>
-    );
+const Item = props => {
+    return <li>{props.value}</li>;
 };
 
 const App = () => {
+    const [range] = useState({ min: 10, max: 10000 });
+    const [n, setN] = useState(0);
+
+    const list = useMemo(() => {
+        const lists = [];
+        for (let i = range.min; i < range.max; i++) {
+            console.log(1);
+            lists.push(
+                <Item
+                    key={i}
+                    value={i}
+                >
+                    {i}
+                </Item>,
+            );
+        }
+        return lists;
+    }, [range.min, range.max]);
+    // const lists = [];
+    // for (let i = range.min; i < range.max; i++) {
+    //     console.log(1);
+    //     lists.push(
+    //         <Item
+    //             key={i}
+    //             value={i}
+    //         >
+    //             {i}
+    //         </Item>,
+    //     );
+    // }
+    // return lists;
+
     return (
         <>
-            <Parent />
+            <ul>{list}</ul>
+            <input
+                type="number"
+                value={n}
+                onChange={e => setN(previous => (previous = e.target.value))}
+            />
         </>
     );
-};
+};;
 
 export default App;
