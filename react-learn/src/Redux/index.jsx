fb179ed1610365d1bf7239e46c5fa3ef3c1e63b2
qishilong <663253062@qq.com>
2022-10-18 20:29:45 +0800

    提交用Redux管理数据

--- react-learn/src/Redux/index.jsx
+++ react-learn/src/Redux/index.jsx
@@ -1,7 +1,4 @@
-import { bindActionCreators, createStore } from "redux";
-
-import { getDecreaseAction, getIncreaseAction, getSetAction } from "./action/action-name";
-import { DECREASE, INCREASE, SET } from "./action/action-type";
+import { createStore } from "redux";
 
 //假设仓库中仅存放了一个数字，该数字的变化可能是+1或-1
 //约定action的格式：{type:"操作类型", payload:附加数据}
@@ -13,33 +10,22 @@ import { DECREASE, INCREASE, SET } from "./action/action-type";
  */
 function reducer(state, action) {
     //返回一个新的状态
-    if (action.type === INCREASE) {
+    if (action.type === "increase") {
         return state + 1;
-    } else if (action.type === DECREASE) {
+    } else if (action.type === "decrease") {
         return state - 1;
-    } else if (action.type === SET) {
-        return action.payload;
     }
     return state; //如果是一个无效的操作类型，数据不变
 }
 
-const store = createStore(reducer, 10);
+window.store = createStore(reducer, 10);
 
-//第一个参数，是action创建函数合并的对象，第二个参数是仓库的dispatch函数
-//得到一个新的对象，新对象中的属性名与第一个参数的属性名一致
-const bindAction = bindActionCreators(
-    { getIncreaseAction, getDecreaseAction, getSetAction },
-    store.dispatch,
-);
+const action = {
+    type: "increase",
+};
 
-console.log(store.getState()); //得到仓库中当前的数据
-//向仓库分发action
-// store.dispatch(getIncreaseAction());
-bindAction.getIncreaseAction(); // 得到一个 increase action, 并直接分发
+console.log(window.store.getState()); //得到仓库中当前的数据
 
-console.log(store.getState());
+window.store.dispatch(action); //向仓库分发action
 
-// store.dispatch(getSetAction(100));
-bindAction.getSetAction(100);
-
-console.log(store.getState()); //得到仓库中当前的数据
+console.log(window.store.getState()); //得到仓库中当前的数据
