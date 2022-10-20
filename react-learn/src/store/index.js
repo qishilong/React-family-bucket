038d664cefe63b35dfc75c2834d604d2258dbea4
qishilong <663253062@qq.com>
2022-10-20 17:37:52 +0800

    提交redux-logger

--- react-learn/src/store/index.js
+++ react-learn/src/store/index.js
@@ -1,80 +1,24 @@
-// import { applyMiddleware, bindActionCreators, createStore } from "redux";
+import logger from "redux-logger";
+
 import { applyMiddleware, bindActionCreators, createStore } from "../reduxSourceCodeAnalysis";
 import { addUserAction, deleteUserAction } from "./action/userAction";
 import reducer from "./reducer";
 
+// import { createLogger } from "redux-logger"; // 打印日志配置
+// const logger = createLogger({
+//     // ... options
+// });
 
 const userAction = {
     add: addUserAction,
     delete: deleteUserAction,
 };
 
-// /**
-//  * 中间件函数
-//  * @param {*} store
-//  * @param {要返回的dispatch} next
-//  * @param {*} action
-//  */
-// function log1(store) {
-//     return function (next) {
-//         console.log(next);
-//         // 以下是要返回的dispatch
-//         return function (action) {
-//             console.log("中间件1");
-//             console.log("旧数据", store.getState());
-//             console.log(action);
-//             next(action);
-//             console.log("新的数据", store.getState());
-//             console.log("");
-//         };
-//     };
-// }
-
-// /**
-//  * 中间件函数
-//  * @param {*} store
-//  * @param {dispatch} next
-//  * @param {*} action
-//  */
-
-// function log2(store) {
-//     return function (next) {
-//         console.log(next);
-//         // 以下是要返回的dispatch
-//         return function (action) {
-//             console.log("中间件1");
-//             console.log("旧数据", store.getState());
-//             console.log(action);
-//             next(action);
-//             console.log("新的数据", store.getState());
-//             console.log("");
-//         };
-//     };
-// }
-
-const log1 = store => next => action => {
-    console.log("中间件1");
-    console.log("旧数据", store.getState());
-    console.log(action);
-    next(action);
-    console.log("新的数据", store.getState());
-    console.log("");
-};
-
-const log2 = store => next => action => {
-    console.log("中间件1");
-    console.log("旧数据", store.getState());
-    console.log(action);
-    next(action);
-    console.log("新的数据", store.getState());
-    console.log("");
-};
-
 // 应用中间件，方式1
-// const store = createStore(reducer, applyMiddleware(log1, log2));
+// const store = createStore(reducer, applyMiddleware(logger));
 
 // 方式2
-const store = applyMiddleware(log1, log2)(createStore)(reducer);
+const store = applyMiddleware(logger)(createStore)(reducer);
 
 const actions = bindActionCreators(userAction, store.dispatch);
 
