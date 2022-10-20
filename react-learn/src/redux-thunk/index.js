4c6436f0cf4900b0d642f578c441d271f639b485
qishilong <663253062@qq.com>
2022-10-20 21:05:45 +0800

    提交手写thunk

---
+++ react-learn/src/redux-thunk/index.js
@@ -0,0 +1,12 @@
+const thunkMiddleware = extra => {
+    return store => next => action => {
+        if (typeof action === "function") {
+            return action(store.dispatch, store.getState(), extra);
+        }
+        return next(action);
+    };
+};
+
+const thunk = thunkMiddleware();
+thunk.withExtraArgument = thunkMiddleware;
+export default thunk;
--- react-learn/src/store/index.js
+++ react-learn/src/store/index.js
@@ -1,11 +1,13 @@
 import logger from "redux-logger";
-import thunk from "redux-thunk";
 
+import thunk from "../redux-thunk";
 import { applyMiddleware, createStore } from "../reduxSourceCodeAnalysis";
 import reducer from "./reducer";
 
+// import thunk from "redux-thunk";
+
 // 应用中间件，方式1
-const store = createStore(reducer, applyMiddleware(thunk, logger), 1);
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 // 方式2
 // const store = applyMiddleware(thunk, logger)(createStore)(reducer);
